59e43fe8d214993a234e0968e27bcc80c7443f64
Jason Stillwell <jstillwell@zendesk.com>
2016-11-10 18:23:24 -0800

    map based flipping

--- keyboard_flip.m
+++ keyboard_flip.m
@@ -40,16 +40,27 @@ BOOL isSpaceReleased(CGEventType type, CGEventRef event) {
   }
 }
 
+NSDictionary *flipMap;
+
+void initializeFlipMap() {
+  flipMap = @{
+    [NSNumber numberWithInt:kVK_ANSI_K]: [NSNumber numberWithInt:kVK_ANSI_D],
+    [NSNumber numberWithInt:kVK_ANSI_D]: [NSNumber numberWithInt:kVK_ANSI_K]
+  };
+}
+
 CGEventRef flip(CGEventRef event) {
 
   CGKeyCode keycode = (CGKeyCode)CGEventGetIntegerValueField(event, kCGKeyboardEventKeycode);
-  if ( keycode == kVK_ANSI_K ) {
-    CGEventSetIntegerValueField(event, kCGKeyboardEventKeycode, (int64_t)kVK_ANSI_D);
+  NSNumber *keynum = [NSNumber numberWithInt:keycode];
+  if ( [flipMap objectForKey:keynum] ) {
+    NSNumber *flippedKeynum = flipMap[keynum];
+    CGKeyCode flippedKeycode = [flippedKeynum intValue];
+    CGEventSetIntegerValueField(event, kCGKeyboardEventKeycode, (int64_t)flippedKeycode);
   }
 
   return event;
 }
-
 
 CGEventRef eventTapFunction(CGEventTapProxy proxy, CGEventType type, CGEventRef event, void *refcon) {
   printf("eventTap triggered\n");
@@ -57,17 +68,21 @@ CGEventRef eventTapFunction(CGEventTapProxy proxy, CGEventType type, CGEventRef 
   if ( isSpacePressed(type,event)  ) {
     isSpaceDown = YES;
     nonSpaceTyped = NO;
+    // TODO We dont send space until its released.
     return event;
   } else if ( isSpaceReleased(type,event) ) {
     isSpaceDown = NO;
     if ( nonSpaceTyped ) {
       // We used space a flip modifier, so we don't type a space.
-      return NULL;
+      // TODO we should squelch this
+      return event; 
     } else {
+
+      // TODO press and release space
       return event;
     }
   } else if ( isSpaceDown ) {
-    if ( type == kCGEventKeyUp ) {
+    if ( type == kCGEventKeyDown ) {
       nonSpaceTyped = YES;
     }
     return flip(event);
@@ -78,6 +93,8 @@ CGEventRef eventTapFunction(CGEventTapProxy proxy, CGEventType type, CGEventRef 
 
 int main(int argc, char** argv) {
   printf("starting\n");
+
+  initializeFlipMap();
 
   machPortRef =  CGEventTapCreate(kCGSessionEventTap,
     kCGTailAppendEventTap,
