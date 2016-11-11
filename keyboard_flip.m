735a0d2ddfb7d31f2ac1afd097c918690ded0d38
Jason Stillwell <jstillwell@zendesk.com>
2016-11-10 17:53:50 -0800

    full version, but I mixed up pressed and released actions

--- keyboard_flip.m
+++ keyboard_flip.m
@@ -1,14 +1,79 @@
 
 #import <Foundation/Foundation.h>
 
+#include <Carbon/Carbon.h>
+
 
 
 CFMachPortRef machPortRef;
 CFRunLoopSourceRef  eventSrc;
+ 
+BOOL isSpaceDown = NO;
+BOOL nonSpaceTyped = NO;
+
+BOOL isSpacePressed(CGEventType type, CGEventRef event) {
+
+  if ( type == kCGEventKeyDown ) {
+
+    CGKeyCode keycode = (CGKeyCode)CGEventGetIntegerValueField(event, kCGKeyboardEventKeycode);
+    if ( keycode == kVK_Space ) {
+      return YES;
+    } else {
+      return NO;
+    }
+  } else  {
+    return NO;
+  }
+}
+
+BOOL isSpaceReleased(CGEventType type, CGEventRef event) {
+
+  if ( type == kCGEventKeyUp ) {
+    CGKeyCode keycode = (CGKeyCode)CGEventGetIntegerValueField(event, kCGKeyboardEventKeycode);
+    if ( keycode == kVK_Space ) {
+      return YES;
+    } else {
+      return NO;
+    }
+  } else {
+    return NO;
+  }
+}
+
+CGEventRef flip(CGEventRef event) {
+
+  CGKeyCode keycode = (CGKeyCode)CGEventGetIntegerValueField(event, kCGKeyboardEventKeycode);
+  if ( keycode == kVK_ANSI_K ) {
+    CGEventSetIntegerValueField(event, kCGKeyboardEventKeycode, (int64_t)kVK_ANSI_D);
+  }
+
+  return event;
+}
+
 
 CGEventRef eventTapFunction(CGEventTapProxy proxy, CGEventType type, CGEventRef event, void *refcon) {
   printf("eventTap triggered\n");
-  return event;
+
+  if ( isSpacePressed(type,event)  ) {
+    isSpaceDown = YES;
+    nonSpaceTyped = NO;
+    return event;
+  } else if ( isSpaceReleased(type,event) ) {
+    isSpaceDown = NO;
+    if ( nonSpaceTyped ) {
+      // We used space a flip modifier, so we don't type a space.
+      return NULL;
+    } else {
+      return event;
+    }
+  } else if ( isSpaceDown ) {
+    if ( type == kCGEventKeyUp ) {
+      nonSpaceTyped = YES;
+    }
+    return flip(event);
+  } else {
+    return event;
+  }
 }
 
 int main(int argc, char** argv) {
@@ -17,7 +82,7 @@ int main(int argc, char** argv) {
   machPortRef =  CGEventTapCreate(kCGSessionEventTap,
     kCGTailAppendEventTap,
     kCGEventTapOptionDefault,
-    CGEventMaskBit(kCGEventKeyDown),
+    CGEventMaskBit(kCGEventKeyDown) | CGEventMaskBit(kCGEventKeyUp),
     (CGEventTapCallBack)eventTapFunction,
     NULL
   );
