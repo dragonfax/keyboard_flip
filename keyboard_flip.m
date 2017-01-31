d366b8ce340d5fc3adc874035e3d46d1c0c062b4
Jason Stillwell <jstillwell@zendesk.com>
2017-01-31 01:11:51 -0800

    partially workes.

--- keyboard_flip.m
+++ keyboard_flip.m
@@ -1,8 +1,13 @@
+#include <IOKit/hid/IOHIDValue.h>
+#include <IOKit/hid/IOHIDLib.h>
+#include <IOKit/hid/IOHIDManager.h>
+#include <IOKit/hidsystem/IOHIDParameter.h>
+#include <IOKit/hidsystem/IOHIDLib.h>
 #import <Foundation/Foundation.h>
 #include <Carbon/Carbon.h>
+#include <unistd.h>
 
-CFMachPortRef machPortRef;
-CFRunLoopSourceRef  eventSrc;
+
  
 BOOL isSpaceDown = NO;
 BOOL nonSpaceTyped = NO;
@@ -39,65 +44,120 @@ BOOL isSpaceReleased(CGEventType type, CGEventRef event) {
 NSDictionary *flipMap;
 NSDictionary *unflipMap;
 
+enum {
+  SCAN_A =  0x04,
+  SCAN_B =  0x05,
+  SCAN_C =  0x06,
+  SCAN_D =  0x07,
+  SCAN_E =  0x08,
+  SCAN_F =  0x09,
+  SCAN_G =  0x0A,
+  SCAN_H =  0x0B,
+  SCAN_I =  0x0C,
+  SCAN_J =  0x0D,
+  SCAN_K =  0x0E,
+  SCAN_L =  0x0F,
+  SCAN_M =  0x10,
+  SCAN_N =  0x11,
+  SCAN_O =  0x12,
+  SCAN_P =  0x13,
+  SCAN_Q =  0x14,
+  SCAN_R =  0x15,
+  SCAN_S =  0x16,
+  SCAN_T =  0x17,
+  SCAN_U =  0x18,
+  SCAN_V =  0x19,
+  SCAN_W =  0x1A,
+  SCAN_X =  0x1B,
+  SCAN_Y =  0x1C,
+  SCAN_Z =  0x1D,
+  SCAN_1 = 0x1E,
+  SCAN_2 = 0x1F,
+  SCAN_3 = 0x20,
+  SCAN_4 = 0x21,
+  SCAN_5 = 0x22,
+  SCAN_6 = 0x23,
+  SCAN_7 = 0x24,
+  SCAN_8 = 0x25,
+  SCAN_9 = 0x26,
+  SCAN_0 = 0x27,
+  SCAN_CapsLock = 0x39,
+  SCAN_Tab = 0x2B,
+  SCAN_Grave = 0x35,
+  SCAN_Shift = 0xE1,
+  SCAN_Semicolon = 0x33,
+  SCAN_Quote = 0x34,
+  SCAN_Return = 0x28,
+  SCAN_LeftBracket = 0x2F,
+  SCAN_RightBracket = 0x30,
+  SCAN_Minus = 0x2D,
+  SCAN_Equal = 0x2E,
+  SCAN_Delete = 0x2A,
+  SCAN_Comma = 0x36,
+  SCAN_Period = 0x37,
+  SCAN_Slash = 0x38,
+  SCAN_Space = 0x2C
+};
+
 void initializeFlipMap() {
   unflipMap = @{
-    [NSNumber numberWithInt:kVK_ANSI_H]: [NSNumber numberWithInt:kVK_ANSI_G],
-    [NSNumber numberWithInt:kVK_ANSI_G]: [NSNumber numberWithInt:kVK_ANSI_F],
-    [NSNumber numberWithInt:kVK_ANSI_F]: [NSNumber numberWithInt:kVK_ANSI_D],
-    [NSNumber numberWithInt:kVK_ANSI_D]: [NSNumber numberWithInt:kVK_ANSI_S],
-    [NSNumber numberWithInt:kVK_ANSI_S]: [NSNumber numberWithInt:kVK_ANSI_A],
-    [NSNumber numberWithInt:kVK_ANSI_A]: [NSNumber numberWithInt:kVK_CapsLock],
+    [NSNumber numberWithInt:SCAN_H]: [NSNumber numberWithInt:SCAN_G],
+    [NSNumber numberWithInt:SCAN_G]: [NSNumber numberWithInt:SCAN_F],
+    [NSNumber numberWithInt:SCAN_F]: [NSNumber numberWithInt:SCAN_D],
+    [NSNumber numberWithInt:SCAN_D]: [NSNumber numberWithInt:SCAN_S],
+    [NSNumber numberWithInt:SCAN_S]: [NSNumber numberWithInt:SCAN_A],
+    [NSNumber numberWithInt:SCAN_A]: [NSNumber numberWithInt:SCAN_CapsLock],
 
-    [NSNumber numberWithInt:kVK_ANSI_Y]: [NSNumber numberWithInt:kVK_ANSI_T],
-    [NSNumber numberWithInt:kVK_ANSI_T]: [NSNumber numberWithInt:kVK_ANSI_R],
-    [NSNumber numberWithInt:kVK_ANSI_R]: [NSNumber numberWithInt:kVK_ANSI_E],
-    [NSNumber numberWithInt:kVK_ANSI_E]: [NSNumber numberWithInt:kVK_ANSI_W],
-    [NSNumber numberWithInt:kVK_ANSI_W]: [NSNumber numberWithInt:kVK_ANSI_Q],
-    [NSNumber numberWithInt:kVK_ANSI_Q]: [NSNumber numberWithInt:kVK_Tab],
+    [NSNumber numberWithInt:SCAN_Y]: [NSNumber numberWithInt:SCAN_T],
+    [NSNumber numberWithInt:SCAN_T]: [NSNumber numberWithInt:SCAN_R],
+    [NSNumber numberWithInt:SCAN_R]: [NSNumber numberWithInt:SCAN_E],
+    [NSNumber numberWithInt:SCAN_E]: [NSNumber numberWithInt:SCAN_W],
+    [NSNumber numberWithInt:SCAN_W]: [NSNumber numberWithInt:SCAN_Q],
+    [NSNumber numberWithInt:SCAN_Q]: [NSNumber numberWithInt:SCAN_Tab],
 
-    [NSNumber numberWithInt:kVK_ANSI_7]: [NSNumber numberWithInt:kVK_ANSI_6],
-    [NSNumber numberWithInt:kVK_ANSI_6]: [NSNumber numberWithInt:kVK_ANSI_5],
-    [NSNumber numberWithInt:kVK_ANSI_5]: [NSNumber numberWithInt:kVK_ANSI_4],
-    [NSNumber numberWithInt:kVK_ANSI_4]: [NSNumber numberWithInt:kVK_ANSI_3],
-    [NSNumber numberWithInt:kVK_ANSI_3]: [NSNumber numberWithInt:kVK_ANSI_2],
-    [NSNumber numberWithInt:kVK_ANSI_2]: [NSNumber numberWithInt:kVK_ANSI_1],
-    [NSNumber numberWithInt:kVK_ANSI_1]: [NSNumber numberWithInt:kVK_ANSI_Grave],
+    [NSNumber numberWithInt:SCAN_7]: [NSNumber numberWithInt:SCAN_6],
+    [NSNumber numberWithInt:SCAN_6]: [NSNumber numberWithInt:SCAN_5],
+    [NSNumber numberWithInt:SCAN_5]: [NSNumber numberWithInt:SCAN_4],
+    [NSNumber numberWithInt:SCAN_4]: [NSNumber numberWithInt:SCAN_3],
+    [NSNumber numberWithInt:SCAN_3]: [NSNumber numberWithInt:SCAN_2],
+    [NSNumber numberWithInt:SCAN_2]: [NSNumber numberWithInt:SCAN_1],
+    [NSNumber numberWithInt:SCAN_1]: [NSNumber numberWithInt:SCAN_Grave],
 
-    [NSNumber numberWithInt:kVK_ANSI_B]: [NSNumber numberWithInt:kVK_ANSI_V],
-    [NSNumber numberWithInt:kVK_ANSI_V]: [NSNumber numberWithInt:kVK_ANSI_C],
-    [NSNumber numberWithInt:kVK_ANSI_C]: [NSNumber numberWithInt:kVK_ANSI_X],
-    [NSNumber numberWithInt:kVK_ANSI_X]: [NSNumber numberWithInt:kVK_ANSI_Z],
-    [NSNumber numberWithInt:kVK_ANSI_Z]: [NSNumber numberWithInt:kVK_Shift],
+    [NSNumber numberWithInt:SCAN_B]: [NSNumber numberWithInt:SCAN_V],
+    [NSNumber numberWithInt:SCAN_V]: [NSNumber numberWithInt:SCAN_C],
+    [NSNumber numberWithInt:SCAN_C]: [NSNumber numberWithInt:SCAN_X],
+    [NSNumber numberWithInt:SCAN_X]: [NSNumber numberWithInt:SCAN_Z],
+    [NSNumber numberWithInt:SCAN_Z]: [NSNumber numberWithInt:SCAN_Shift],
   };
 
   flipMap = @{
-    [NSNumber numberWithInt:kVK_ANSI_G]: [NSNumber numberWithInt:kVK_ANSI_J],
-    [NSNumber numberWithInt:kVK_ANSI_F]: [NSNumber numberWithInt:kVK_ANSI_K],
-    [NSNumber numberWithInt:kVK_ANSI_D]: [NSNumber numberWithInt:kVK_ANSI_L],
-    [NSNumber numberWithInt:kVK_ANSI_S]: [NSNumber numberWithInt:kVK_ANSI_Semicolon],
-    [NSNumber numberWithInt:kVK_ANSI_A]: [NSNumber numberWithInt:kVK_ANSI_Quote],
-    [NSNumber numberWithInt:kVK_CapsLock]: [NSNumber numberWithInt:kVK_Return],
+    [NSNumber numberWithInt:SCAN_G]: [NSNumber numberWithInt:SCAN_J],
+    [NSNumber numberWithInt:SCAN_F]: [NSNumber numberWithInt:SCAN_K],
+    [NSNumber numberWithInt:SCAN_D]: [NSNumber numberWithInt:SCAN_L],
+    [NSNumber numberWithInt:SCAN_S]: [NSNumber numberWithInt:SCAN_Semicolon],
+    [NSNumber numberWithInt:SCAN_A]: [NSNumber numberWithInt:SCAN_Quote],
+    [NSNumber numberWithInt:SCAN_CapsLock]: [NSNumber numberWithInt:SCAN_Return],
 
-    [NSNumber numberWithInt:kVK_ANSI_T]: [NSNumber numberWithInt:kVK_ANSI_U],
-    [NSNumber numberWithInt:kVK_ANSI_R]: [NSNumber numberWithInt:kVK_ANSI_I],
-    [NSNumber numberWithInt:kVK_ANSI_E]: [NSNumber numberWithInt:kVK_ANSI_O],
-    [NSNumber numberWithInt:kVK_ANSI_W]: [NSNumber numberWithInt:kVK_ANSI_P],
-    [NSNumber numberWithInt:kVK_ANSI_Q]: [NSNumber numberWithInt:kVK_ANSI_LeftBracket],
-    [NSNumber numberWithInt:kVK_Tab]: [NSNumber numberWithInt:kVK_ANSI_RightBracket],
+    [NSNumber numberWithInt:SCAN_T]: [NSNumber numberWithInt:SCAN_U],
+    [NSNumber numberWithInt:SCAN_R]: [NSNumber numberWithInt:SCAN_I],
+    [NSNumber numberWithInt:SCAN_E]: [NSNumber numberWithInt:SCAN_O],
+    [NSNumber numberWithInt:SCAN_W]: [NSNumber numberWithInt:SCAN_P],
+    [NSNumber numberWithInt:SCAN_Q]: [NSNumber numberWithInt:SCAN_LeftBracket],
+    [NSNumber numberWithInt:SCAN_Tab]: [NSNumber numberWithInt:SCAN_RightBracket],
 
-    [NSNumber numberWithInt:kVK_ANSI_6]: [NSNumber numberWithInt:kVK_ANSI_7],
-    [NSNumber numberWithInt:kVK_ANSI_5]: [NSNumber numberWithInt:kVK_ANSI_8],
-    [NSNumber numberWithInt:kVK_ANSI_4]: [NSNumber numberWithInt:kVK_ANSI_9],
-    [NSNumber numberWithInt:kVK_ANSI_3]: [NSNumber numberWithInt:kVK_ANSI_0],
-    [NSNumber numberWithInt:kVK_ANSI_2]: [NSNumber numberWithInt:kVK_ANSI_Minus],
-    [NSNumber numberWithInt:kVK_ANSI_1]: [NSNumber numberWithInt:kVK_ANSI_Equal],
-    [NSNumber numberWithInt:kVK_ANSI_Grave]: [NSNumber numberWithInt:kVK_Delete],
+    [NSNumber numberWithInt:SCAN_6]: [NSNumber numberWithInt:SCAN_7],
+    [NSNumber numberWithInt:SCAN_5]: [NSNumber numberWithInt:SCAN_8],
+    [NSNumber numberWithInt:SCAN_4]: [NSNumber numberWithInt:SCAN_9],
+    [NSNumber numberWithInt:SCAN_3]: [NSNumber numberWithInt:SCAN_0],
+    [NSNumber numberWithInt:SCAN_2]: [NSNumber numberWithInt:SCAN_Minus],
+    [NSNumber numberWithInt:SCAN_1]: [NSNumber numberWithInt:SCAN_Equal],
+    [NSNumber numberWithInt:SCAN_Grave]: [NSNumber numberWithInt:SCAN_Delete],
 
-    [NSNumber numberWithInt:kVK_ANSI_B]: [NSNumber numberWithInt:kVK_ANSI_N],
-    [NSNumber numberWithInt:kVK_ANSI_V]: [NSNumber numberWithInt:kVK_ANSI_M],
-    [NSNumber numberWithInt:kVK_ANSI_C]: [NSNumber numberWithInt:kVK_ANSI_Comma],
-    [NSNumber numberWithInt:kVK_ANSI_X]: [NSNumber numberWithInt:kVK_ANSI_Period],
-    [NSNumber numberWithInt:kVK_ANSI_Z]: [NSNumber numberWithInt:kVK_ANSI_Slash],
+    [NSNumber numberWithInt:SCAN_B]: [NSNumber numberWithInt:SCAN_N],
+    [NSNumber numberWithInt:SCAN_V]: [NSNumber numberWithInt:SCAN_M],
+    [NSNumber numberWithInt:SCAN_C]: [NSNumber numberWithInt:SCAN_Comma],
+    [NSNumber numberWithInt:SCAN_X]: [NSNumber numberWithInt:SCAN_Period],
+    [NSNumber numberWithInt:SCAN_Z]: [NSNumber numberWithInt:SCAN_Slash],
   };
 }
 
@@ -122,8 +182,6 @@ CGEventRef flip(BOOL flipped, CGEventRef event) {
   return event;
 }
 
-CGEventSourceRef src;
-
 CGEventRef eventTapFunction(CGEventTapProxy proxy, CGEventType type, CGEventRef event, void *refcon) {
 
   if ( isSpacePressed(type,event)  ) {
@@ -146,6 +204,7 @@ CGEventRef eventTapFunction(CGEventTapProxy proxy, CGEventType type, CGEventRef 
     } else {
 
       // TODO press and release space
+      CGEventSourceRef src;
       CGEventRef newEvent = CGEventCreateKeyboardEvent(src, kVK_Space, true);
       return newEvent;
     }
@@ -159,34 +218,128 @@ CGEventRef eventTapFunction(CGEventTapProxy proxy, CGEventType type, CGEventRef 
   }
 }
 
-int main(int argc, char** argv) {
+static io_connect_t get_event_driver(void) {
+  static mach_port_t sEventDrvrRef = 0;
+  mach_port_t masterPort, service, iter;
+  kern_return_t kr;
 
-  initializeFlipMap();
+  if (!sEventDrvrRef) {
+    // Get master device port
+    kr = IOMasterPort(bootstrap_port, &masterPort);
+    check(KERN_SUCCESS == kr);
 
-  src = CGEventSourceCreate(kCGEventSourceStateHIDSystemState);
+    kr = IOServiceGetMatchingServices(masterPort, IOServiceMatching("IOHIDSystem"), &iter); // or "IOHIDKeyboard"
+    check(KERN_SUCCESS == kr);
 
-  machPortRef =  CGEventTapCreate(kCGSessionEventTap,
-    kCGTailAppendEventTap,
-    kCGEventTapOptionDefault,
-    CGEventMaskBit(kCGEventKeyDown) | CGEventMaskBit(kCGEventKeyUp),
-    (CGEventTapCallBack)eventTapFunction,
-    NULL
-  );
+    service = IOIteratorNext(iter);
+    check(service);
 
-  if (machPortRef == NULL) {
-      printf("CGEventTapCreate failed!\n");
-  } else {
-      eventSrc = CFMachPortCreateRunLoopSource(NULL, machPortRef, 0);
-      if ( eventSrc == NULL ) {
-          printf( "No event run loop src?\n" );
-      }else {
-        CFRunLoopRef runLoop =  CFRunLoopGetCurrent(); //GetCFRunLoopFromEventLoop(GetMainEventLoop ()); 
+    kr = IOServiceOpen(service, mach_task_self(), kIOHIDParamConnectType, &sEventDrvrRef);
+    check(KERN_SUCCESS == kr);
 
-        // Get the CFRunLoop primitive for the Carbon Main Event Loop, and add the new event souce
-        CFRunLoopAddSource(runLoop, eventSrc, kCFRunLoopDefaultMode);
-
-      }
+    IOObjectRelease(service);
+    IOObjectRelease(iter);
   }
+  return sEventDrvrRef;
+}
 
-  CFRunLoopRun();
+
+void myHIDKeyboardCallback( void* context,  IOReturn result,  void* sender,  IOHIDValueRef value )
+{
+    IOHIDElementRef elem = IOHIDValueGetElement( value );
+
+    if (IOHIDElementGetUsagePage(elem) != 0x07)
+        return;
+
+    uint32_t scancode = IOHIDElementGetUsage( elem );
+
+    if (scancode < 4 || scancode > 231)
+        return;
+
+    long pressed = IOHIDValueGetIntegerValue( value );
+
+    printf( "scancode: %d, pressed: %ld\n", scancode, pressed );
+
+
+
+    IOGPoint loc = {0, 0};
+    NXEventData event;
+    bzero(&event, sizeof(NXEventData));
+    event.key.repeat = FALSE;
+    event.key.keyCode = scancode;
+    event.key.origCharSet = event.key.charSet = NX_ASCIISET;
+    event.key.origCharCode = event.key.charCode = 0; // ?!
+
+    IOReturn kr = IOHIDPostEvent(get_event_driver(), pressed ? NX_KEYDOWN : NX_KEYUP, loc, &event, kNXEventDataVersion, 0, TRUE );
+    if ( kr != kIOReturnSuccess) {
+      printf("failure to post event (%x)\n", err_get_code(kr));
+    }
+
+}
+
+
+CFMutableDictionaryRef myCreateDeviceMatchingDictionary( UInt32 usagePage,  UInt32 usage )
+{
+    CFMutableDictionaryRef dict = CFDictionaryCreateMutable(
+                                                            kCFAllocatorDefault, 0
+                                                        , & kCFTypeDictionaryKeyCallBacks
+                                                        , & kCFTypeDictionaryValueCallBacks );
+    if ( ! dict )
+        return NULL;
+
+    CFNumberRef pageNumberRef = CFNumberCreate( kCFAllocatorDefault, kCFNumberIntType, & usagePage );
+    if ( ! pageNumberRef ) {
+        CFRelease( dict );
+        return NULL;
+    }
+
+    CFDictionarySetValue( dict, CFSTR(kIOHIDDeviceUsagePageKey), pageNumberRef );
+    CFRelease( pageNumberRef );
+
+    CFNumberRef usageNumberRef = CFNumberCreate( kCFAllocatorDefault, kCFNumberIntType, & usage );
+
+    if ( ! usageNumberRef ) {
+        CFRelease( dict );
+        return NULL;
+    }
+
+    CFDictionarySetValue( dict, CFSTR(kIOHIDDeviceUsageKey), usageNumberRef );
+    CFRelease( usageNumberRef );
+
+    return dict;
+}
+
+
+int main(void)
+{
+    initializeFlipMap();
+
+    IOHIDManagerRef hidManager = IOHIDManagerCreate( kCFAllocatorDefault, kIOHIDOptionsTypeNone );
+
+    CFArrayRef matches;
+    {
+        CFMutableDictionaryRef keyboard = myCreateDeviceMatchingDictionary( 0x01, 6 );
+        CFMutableDictionaryRef keypad   = myCreateDeviceMatchingDictionary( 0x01, 7 );
+
+        CFMutableDictionaryRef matchesList[] = { keyboard, keypad };
+
+        matches = CFArrayCreate( kCFAllocatorDefault, (const void **)matchesList, 2, NULL );
+    }
+
+    IOHIDManagerSetDeviceMatchingMultiple( hidManager, matches );
+
+    IOHIDManagerRegisterInputValueCallback( hidManager, myHIDKeyboardCallback, NULL );
+
+    IOHIDManagerScheduleWithRunLoop( hidManager, CFRunLoopGetMain(), kCFRunLoopDefaultMode );
+
+    // IOReturn r = IOHIDManagerOpen( hidManager, kIOHIDOptionsTypeSeizeDevice);
+    IOReturn r = IOHIDManagerOpen( hidManager, kIOHIDOptionsTypeSeizeDevice);
+    if ( r != kIOReturnSuccess ) {
+      printf("failure to open device in exclusive %x", err_get_code(r));
+      exit(1);
+    }
+
+    seteuid(502);
+
+    CFRunLoopRun(); // spins
 }
